db57826917bd5852a966757e1d039492d6314aff
sabari-D-raj <shabariraj9@gmail.com>
2026-05-29 18:44:34 +0530

    pallindrome

---
+++ pallindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        rev=0
+        num=x
+        if x<0:
+            return False
+        while(x>0):
+            rem=x%10
+            rev=rev*10+rem
+            x=x//10
+        return rev==num
+
+        
